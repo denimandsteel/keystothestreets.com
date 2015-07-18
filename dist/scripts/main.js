e9d199fd23c3ec4f0fcf4f9e058c58c8f6c78f61
Tylor Sherman <tylor@denimandsteel.com>
2015-07-17 18:02:10 -0700

    New pianos!

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -345,6 +345,7 @@ $(function() {
     'nelson',
     'lot-19',
     'aquarium',
+    'travelling',
   ];
   var index = 0;
 
--- js/script.js
+++ js/script.js
@@ -151,6 +151,7 @@ $(function() {
     'nelson',
     'lot-19',
     'aquarium',
+    'travelling',
   ];
   var index = 0;
 
